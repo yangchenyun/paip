4f8cc75db74fe739d86970266335886b6aa0c9c2
Steven Yang <yangchenyun@gmail.com>
2023-03-07 23:56:55 -0800

    Export find-all and find-all-if

--- utils.lisp
+++ utils.lisp
@@ -1,5 +1,8 @@
-(defpackage :utils
-    (:export mappend cross-product combine-all))
+(defpackage #:utils
+    (:use #:cl)
+    (:export :mappend :cross-product :combine-all :find-all :find-all-if))
+    
+(in-package #:utils)
 
 (defun mappend (fn the-list)
     "Apply fn to the list and append the results"
@@ -11,3 +14,11 @@
 (defun combine-all (xlist ylist)
   "Return a list of lists formed by appending a y to an x"
   (cross-product #'append xlist ylist))
+
+(defun find-all (item sequence &rest keyword-args 
+                 &key (test #'eql) test-not &allow-other-keys)
+    (if test-not
+        (apply #'remove item sequence :test-not (complement test-not) keyword-args)
+        (apply #'remove item sequence :test (complement test) keyword-args)))
+
+(setf (symbol-function 'find-all-if) #'remove-if-not)
